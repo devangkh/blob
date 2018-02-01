88bf6a60e8cc2741c94d832a1d76271f009d17d2
siddharth <siddharthjhanwar@yahoo.in>
2018-02-02 00:03:45 +0530

    polarity analysis added

--- ideation/blob/urls.py
+++ ideation/blob/urls.py
@@ -1,1 +1,9 @@
-ff
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls import url
+
+
+
+urlpatterns = [
+    url(r'^sentiment/polarity/(?P<text>[-\w]+)$', django_view,),
+]
