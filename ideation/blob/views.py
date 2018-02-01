642869faf538e579b02d71b00ae7e2b713270651
siddharth <siddharthjhanwar@yahoo.in>
2018-02-02 00:02:26 +0530

    polarity analysis added

--- ideation/blob/views.py
+++ ideation/blob/views.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
+import requests
 # Create your views here.
+
+
+from textblob import TextBlob
+
+
+def django_view(request, text):
+    # get the response from the URL
+    testimonial = TextBlob(text)
+
+    response = testimonial.sentiment.polarity
+    return HttpResponse(response)
